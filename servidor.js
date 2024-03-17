f7b34041c152bfcfe7f5993d501cfb4bcf20cc4e
Márcio Diniz <marcio.sccp10@gmail.com>
2024-03-17 11:57:48 -0300

    Updates

--- servidor.js
+++ servidor.js
@@ -11,7 +11,7 @@ var servidor = app.listen(8080, function() {
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    fs.readFile('documento.html', function(erro, dado) {
+    fs.readFile('formulario.html', function(erro, dado) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(dado);
         res.end();
